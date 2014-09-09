29256a50977dda8515cd92568d3f44f02a47e0ae
Daniel Martinez <dmartz86@gmail.com>
2014-09-09 12:01:27 -0500

    Updating slides controller and seed

--- controllers/slides.js
+++ controllers/slides.js
@@ -1,31 +1,34 @@
 var MPill = require('mpill').MPill;
 var Config = require('../config');
+var model = new MPill('slides', Config.URL);
 
 /**
   *Slides controller
   */
-var Slides = function(){
-  this.model = new MPill('slides', Config.URL);
-};
+var Slides = function(){};
 
 Slides.prototype.List = function(req, res){
-  return this.model.Find();
+  res.json(model.Find({}));
 };
 
 Slides.prototype.Create = function(req, res){
-  return this.model.Create();
+  return model.Create({});
 };
 
 Slides.prototype.Update = function(req, res){
-  return this.model.Update();
+  return model.Update();
 };
 
 Slides.prototype.Get = function(req, res){
-  return this.model.Find();
+  return model.Find();
 };
 
 Slides.prototype.Remove = function(req, res){
-  return this.model.Update();
+  return model.Update();
+};
+
+Slides.prototype.Seed = function(req, res){
+  return model.Update();
 };
 
 module.exports.Slides = Slides;
